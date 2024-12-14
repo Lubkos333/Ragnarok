9e2a8cb4159c91a59093d6746153af5ea4f2a2ed
Bruno Semerek <bruneksemerek@seznam.cz>
2024-12-14 16:54:54 +0100

    RAG-23 : create app structure

---
+++ Ragnarok-Data-App/DocumentApi/src/main/java/com/documentapi/DocumentApi.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.documentapi;
+
+/**
+ *
+ * @author brune
+ */
+public class DocumentApi {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
