38d2a2a9ecc55b6dda5cd1dfa46f690270ff1c47
Bruno Semerek <bruneksemerek@seznam.cz>
2024-12-14 17:12:43 +0100

    RAG-23 : add controller for endpoints

--- Ragnarok-Data-App/DocumentApi/src/main/java/com/documentapi/DocumentApi.java
+++ Ragnarok-Data-App/DocumentApi/src/main/java/com/documentapi/DocumentApi.java
@@ -4,13 +4,26 @@
 
 package com.documentapi;
 
+import java.util.Collections;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+
 /**
  *
  * @author brune
  */
+@SpringBootApplication
+@ConfigurationPropertiesScan
+@EnableAsync
 public class DocumentApi {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args){
+        SpringApplication app = new SpringApplication(DocumentApi.class);
+        app.setDefaultProperties(Collections
+          .singletonMap("server.port", "9090"));
+        app.run(args);
     }
 }
+
