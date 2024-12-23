a571f26ae478e66f0c6f4d79dd27bf919dffa66c
Bruno Semerek <bruneksemerek@seznam.cz>
2024-12-23 14:02:44 +0100

    proper exception hadling when no document found

---
+++ Ragnarok-Data-App/DocumentApi/src/main/java/com/documentapi/Exception/DocumentNotFoundException.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.documentapi.Exception;
+
+/**
+ *
+ * @author brune
+ */
+public class DocumentNotFoundException extends Exception{
+     public DocumentNotFoundException(String message) {
+        super(message);
+    }
+}
