3075d25b9097025a05a8fcfb404dceb5507d141e
ChytraFilip <chytrf00@jcu.cz>
2025-01-05 00:07:34 +0100

    feat: setup the initial project boilerplate
    delete the default page
    add shadcn/ui components
    set up app routes

---
+++ Ragnarok-UI/ragnarok/src/app/chat/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const chat = () => {
+  return (
+    <div>
+      <h1 className="text-4xl">Chat Page</h1>
+      <p>This is the chat page content.</p>
+    </div>
+  );
+};
+
+export default chat;
