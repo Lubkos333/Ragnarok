ac8eb3046e3407390439c3733d0a5717afca1bcf
lubos zeman <lubos.zeman@redrobot.cz>
2025-01-13 15:31:46 +0100

    Connect user on backend via WS (localhost only)

--- Ragnarok-UI/ragnarok/next.config.ts
+++ Ragnarok-UI/ragnarok/next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/ws/chat", 
+        destination: "http://localhost:7777/ws/chat",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
