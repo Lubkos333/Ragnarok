5cce858d3b4c3ef7fe9b0d491ccfd24ae2238cf4
lubos zeman <lubos.zeman@redrobot.cz>
2025-04-12 18:45:07 +0200

    reabuild

--- Ragnarok-RAG-App/src/main/java/cz/ragnarok/ragnarok/service/DataService.java
+++ Ragnarok-RAG-App/src/main/java/cz/ragnarok/ragnarok/service/DataService.java
@@ -2,6 +2,7 @@ package cz.ragnarok.ragnarok.service;
 
 import cz.ragnarok.ragnarok.rest.dto.ChunkDto;
 import org.springframework.ai.document.Document;
+import org.springframework.ai.retry.NonTransientAiException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.core.ParameterizedTypeReference;
@@ -10,7 +11,9 @@ import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class DataService {
@@ -22,7 +25,7 @@ public class DataService {
     @Autowired
     private VectorDBService vectorDBService;
 
-    public Mono getChunks(String url) {
+    public Mono<List<ChunkDto>> getChunks(String url) {
         return dataClient.get()
                 .uri("/api/processing/getChunks?url="+url)
                 .header("Authorization", "Bearer testApiKey")
@@ -31,8 +34,24 @@ public class DataService {
                 .timeout(Duration.ofMinutes(5));
     }
 
+    public Mono<List<ChunkDto>> getParagraphsByDesignation(String designation) {
+        return dataClient.get()
+                .uri("/api/processing/getParagraphsByDesignation?designation="+ designation)
+                .header("Authorization", "Bearer testApiKey")
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<List<ChunkDto>>() {})
+                .timeout(Duration.ofMinutes(5));
+    }
     public List<Document> chunksToDocuments(Mono<List<ChunkDto>> chunks) {
-        List<Document> docs = chunks.block().subList(0,10).stream().map(
+        List<Document> docs = chunks.block().stream().skip(30).map(
+                chunkDto -> vectorDBService.makeDocument(chunkDto)
+        ).toList();
+
+        return docs;
+    }
+
+    public List<Document> chunksToDocuments(List<ChunkDto> chunks) {
+        List<Document> docs = chunks.stream().map(
                 chunkDto -> vectorDBService.makeDocument(chunkDto)
         ).toList();
 
@@ -40,13 +59,67 @@ public class DataService {
     }
 
     public void saveChunks() {
-        List<Document> documents = chunksToDocuments(
-                getChunks("https://www.e-sbirka.cz/souborove-sluzby/soubory/c69e52bb-26a3-4516-8088-85a3d88cafc1")
-        );
-        int chunkSize = 100;
-        for (int i = 0; i < documents.size(); i += chunkSize){
-            List<Document> chunk = documents.subList(i, Math.min(i + chunkSize, documents.size()));
-            vectorDBService.upload(chunk);
+        //List<ChunkDto> chunks = getChunks("https://www.e-sbirka.cz/souborove-sluzby/soubory/c69e52bb-26a3-4516-8088-85a3d88cafc1").block();
+        List<ChunkDto> chunks = getParagraphsByDesignation("89/2012 Sb.").block();
+        chunks = chunks.stream().toList();
+        chunks = chunks.subList(0, chunks.size() - 1);
+        int chunksSize = chunks.size();
+        int chunkSize = 10;
+        for (int i = 0; i < chunksSize; i += chunkSize){
+            try {
+                List<ChunkDto> chunk = chunks.subList(i, Math.min(i + chunkSize, chunksSize));
+                vectorDBService.uploadChunks(chunksToDocuments(chunk));
+            }
+            catch (Exception e) {
+                System.out.println(e);
+                /*List<ChunkDto> chunk = chunks.subList(i, Math.min((i + chunkSize) -1 , chunksSize -1 ));
+                vectorDBService.uploadChunks(chunksToDocuments(chunk));*/
+            }
+        }
+    }
+
+    public void saveShortenChunks() {
+        //List<ChunkDto> chunks = getChunks("https://www.e-sbirka.cz/souborove-sluzby/soubory/c69e52bb-26a3-4516-8088-85a3d88cafc1").block();
+        List<ChunkDto> chunks = getParagraphsByDesignation("89/2012 Sb.").block();
+        chunks = chunks.stream().toList();
+        int chunksSize = chunks.size();
+        int chunkSize = 10;
+        for (int i = 0; i < chunksSize; i += chunkSize){
+            upload(chunks.subList(i, Math.min(i + chunkSize, chunksSize)));
+        }
+    }
+
+    private void upload(List<ChunkDto> chunks) {
+        try {
+            vectorDBService.uploadChunks(chunksToDocuments(chunks));
+        }
+        catch (NonTransientAiException e) {
+            List<ChunkDto> shortenChunks = new ArrayList<>();
+            for (ChunkDto chunkDto : chunks) {
+                shortenChunks.addAll(shortChunk(chunkDto));
+            }
+            upload(shortenChunks);
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    private List<ChunkDto> shortChunk(ChunkDto chunkDto) {
+        int chunkLength = chunkDto.getContent().length();
+        int contentLength = 400;
+        if(chunkLength < contentLength) {
+            return List.of(chunkDto);
+        }
+        else if(chunkLength > contentLength && chunkLength < contentLength * 2) {
+            ChunkDto firstPart = new ChunkDto(chunkDto);
+            ChunkDto secondPart = new ChunkDto(chunkDto);
+            firstPart.setContent(firstPart.getContent().substring(0,contentLength));
+            secondPart.setContent(secondPart.getContent().substring(Math.min(chunkLength - contentLength, contentLength)));
+            return List.of(firstPart, secondPart);
+        }
+        else {
+            return List.of();
         }
     }
 
