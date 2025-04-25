28a165019d17eb8035f85097bc6581628df80489
brune <bruneksemerek@seznam.cz>
2025-04-25 10:20:11 +0200

    introduce styles configuration through properties

---
+++ Ragnarok-Data-App/DocumentApi/src/main/java/com/documentapi/Configuration/KeywordsConfig.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.documentapi.Configuration;
+
+import java.util.List;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author brune
+ */
+
+@ConfigurationProperties(prefix = "keywords")
+public record KeywordsConfig(
+    List<String> list
+) {}
---
+++ Ragnarok-Data-App/DocumentApi/src/main/java/com/documentapi/Configuration/StyleConfig.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.documentapi.Configuration;
+
+import java.util.List;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+import org.springframework.stereotype.Component;
+
+@ConfigurationProperties(prefix = "style")
+public record StyleConfig(
+    List<String> head,
+    List<String> main,
+    List<String> subtitle,
+    List<String> section,
+    List<String> part,
+    List<String> title,
+    List<String> newchunk
+) {}
