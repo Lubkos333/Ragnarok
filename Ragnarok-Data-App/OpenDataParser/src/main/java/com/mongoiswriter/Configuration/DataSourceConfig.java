b98bdafeff5e0d20a5100d03018c34304f63c38e
brune <bruneksemerek@seznam.cz>
2025-05-08 14:37:48 +0200

    cleanup

--- Ragnarok-Data-App/OpenDataParser/src/main/java/com/mongoiswriter/Configuration/DataSourceConfig.java
+++ Ragnarok-Data-App/OpenDataParser/src/main/java/com/mongoiswriter/Configuration/DataSourceConfig.java
@@ -18,16 +18,8 @@ import org.springframework.stereotype.Component;
 @ConfigurationPropertiesScan
 @EnableAsync
 public class DataSourceConfig {
-    @Value("${sbirka.url.terminy.base}")
-    public String URL_TERMINY_BASE;
-    @Value("${sbirka.url.terminy.popis}")
-    public String URL_TERMINY_POPIS;   
-    @Value("${sbirka.url.terminy.vazba}")
-    public String URL_TERMINY_VAZBA;
     @Value("${sbirka.url.akty.zneni}")
     public String URL_AKTY_ZNENI; 
-    @Value("${sbirka.url.akty.vazba}")
-    public String URL_AKTY_VAZBA; 
     @Value("${sbirka.documents.number}")
     public int DOCUMENTS_NUMBER;
 }
